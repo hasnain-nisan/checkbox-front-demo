cf5d14363dd33133be21d418a721fc3bb02a0476
Hasnain Ahamed Nisan <hasnain.nisan@outlook.com>
2023-08-27 14:24:34 +0600

    pp

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Filter from './pages/Filter';
 
 const App = () => {
     // const accessToken = '51|mcDG2AWWgN7m1oAlH5I4E9wQ0GB82eMvePsNUMnB';
-    axios.defaults.baseURL = 'https://phpstack-924353-3259330.cloudwaysapps.com/api/v2/';
+    axios.defaults.baseURL = 'https://phpstack-924353-3259330.cloudwaysapps.com/api/v2';
     // axios.defaults.baseURL = 'http://localhost/checkbox-v2/api/v2/';
     // axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
     axios.defaults.headers.common['Checkbox-Api-V2-Key'] = '83324867-6668-4c04-bf36-91714ea8b3e3';
